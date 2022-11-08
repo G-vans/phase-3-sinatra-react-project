9b5dcc608bbcb444ad94fd65fc427ca1c8830ee4
Derrick Nyasio <dnyasio2014@gmail.com>
2022-11-08 16:26:48 +0300

    feat: creates movies table in migration

--- db/migrate/20221108131436_create_movies_table.rb
+++ db/migrate/20221108131436_create_movies_table.rb
@@ -1,5 +1,9 @@
 class CreateMoviesTable < ActiveRecord::Migration[6.1]
   def change
-
+    create_table :movies do |t|
+      t.string :name
+      t.string :image_url
+      t.text :overview
+    end
   end
 end
